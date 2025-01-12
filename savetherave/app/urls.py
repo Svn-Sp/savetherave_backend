4ed79ae17e4eb1104c4bf7f131d23d3727095a95
jmjumper <juli.springer@gmx.de>
2025-01-11 17:32:27 -0800

    it's time to find friends

--- savetherave/app/urls.py
+++ savetherave/app/urls.py
@@ -6,9 +6,11 @@ from app import views
 from savetherave import settings
 
 urlpatterns = [
+    # path("user/friends_info", views.friends_info),
     path("user/<int:id>", views.user_info),
     path("user/create", views.CreateUserView.as_view()),
     path("user/set_profile_picture", views.set_profile_picture),
+    path("user/add_friend", views.AddFriendView.as_view()),
     path("party/create", views.create_party),
     path("party/<int:id>", views.get_party_info),
 ]
