83cc27bbfb7acb7f05b1ce0922985932db285f6b
Sven Spehr <spehr.sv@gmail.com>
2025-01-11 18:47:27 -0800

    feat: finish party

--- savetherave/app/urls.py
+++ savetherave/app/urls.py
@@ -6,9 +6,10 @@ from app import views
 from savetherave import settings
 
 urlpatterns = [
-    path("user/<int:id>", views.user_info),
-    path("user/create", views.CreateUserView.as_view()),
-    path("user/set_profile_picture", views.set_profile_picture),
-    path("party/create", views.create_party),
-    path("party/<int:id>", views.get_party_info),
+    path("user/", views.token_based_user_info),
+    path("user/<int:id>/", views.user_info),
+    path("user/create/", views.CreateUserView.as_view()),
+    path("user/set_profile_picture/", views.set_profile_picture),
+    path("party/create/", views.create_party),
+    path("party/<int:id>/", views.get_party_info),
 ]
